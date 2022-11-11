bdf9bb08c27b63d61e4c5d232554964c0c1f58c9
Jaedson Silva <contacanal798@gmail.com>
2022-11-10 21:31:10 -0300

    adding setup script to build project

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    name='mathiz',
+    description='Mathiz, a complete web framework',
+    url='https://github.com/firlast/mathiz'
+)
