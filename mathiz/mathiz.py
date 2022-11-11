ca6050a388a98e47d1a7275e0d8878ff2e78dcd3
Jaedson Silva <contacanal798@gmail.com>
2022-11-10 22:07:14 -0300

    adding method to process request with threads

--- mathiz/mathiz.py
+++ mathiz/mathiz.py
@@ -4,6 +4,8 @@ from wsblib import route
 from wsblib import server
 from wsblib import request
 
+from http_pyparser import response
+
 
 class Mathiz:
     def __init__(self) -> None:
@@ -17,6 +19,16 @@ class Mathiz:
             self._routes.append(_route)
 
         return decorator
+
+    def _process(self, client: server.Client) -> None:
+        result = self._process_request.process(client)
+
+        if result:
+            _response, _request = result
+            final_response = response.make_response(_response)
+
+            client.send_message(final_response)
+            client.destroy()
 
     def run(self, host: str = '127.0.0.1', port: int = 5500) -> None:
         _server = server.Server()
