df5a9822f301386f9207eef65e4a3d3b05f27180
Jaedson Silva <contacanal798@gmail.com>
2022-11-10 22:29:24 -0300

    adding long_description to setup script

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+with open('README.md', 'r') as reader:
+    readme = reader.read()
+
 setup(
     author='Jaedson Silva',
     author_email='imunknowuser@protonmail.com',
     name='mathiz',
     description='Mathiz, a complete web framework',
+    long_description=readme,
+    long_description_content_type='text/markdown',
     version='0.1.0',
     url='https://github.com/firlast/mathiz',
     packages=['mathiz'],
