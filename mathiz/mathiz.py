8cdbf2f871cd5e3f96f2ba5c35fbe9aabff4bbd2
Jaedson Silva <contacanal798@gmail.com>
2022-11-10 21:54:38 -0300

    adding method to register routes

--- mathiz/mathiz.py
+++ mathiz/mathiz.py
@@ -1,7 +1,16 @@
-import wsblib
+from types import FunctionType
+
+from wsblib import route
 
 
 class Mathiz:
     def __init__(self) -> None:
         self._routes = []
         self._errors_callback = []
+
+    def route(self, path: str, method: tuple = ('GET',)) -> FunctionType:
+        def decorator(func):
+            _route = route.Route(func, path, method)
+            self._routes.append(_route)
+
+        return decorator
