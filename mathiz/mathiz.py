3833de34c711d885b723e59b4f94a4fd7afd49fd
Jaedson Silva <contacanal798@gmail.com>
2022-11-10 22:01:09 -0300

    creating ProcessRequest instance on run server

--- mathiz/mathiz.py
+++ mathiz/mathiz.py
@@ -2,12 +2,14 @@ from types import FunctionType
 
 from wsblib import route
 from wsblib import server
+from wsblib import request
 
 
 class Mathiz:
     def __init__(self) -> None:
         self._routes = []
         self._errors_callback = []
+        self._process_request: request.ProcessRequest = None
 
     def route(self, path: str, methods: tuple = ('GET',)) -> FunctionType:
         def decorator(func):
@@ -19,6 +21,10 @@ class Mathiz:
     def run(self, host: str = '127.0.0.1', port: int = 5500) -> None:
         _server = server.Server()
         _server.start(host, port)
+
+        self._process_request = request.ProcessRequest(
+            self._routes, self._errors_callback
+        )
         
         while True:
             client = _server.wait_client()
