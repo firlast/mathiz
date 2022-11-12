3a51ac35f23042018a1525f8ffc8fae1f62e2e13
Jaedson Silva <contacanal798@gmail.com>
2022-11-12 18:45:59 -0300

    creating RequestData instance in __init__

--- mathiz/__init__.py
+++ mathiz/__init__.py
@@ -1,3 +1,9 @@
+from wsblib.request import RequestData
+from http_pyparser.parser import HTTPData
+
 from .mathiz import Mathiz
+
+http_data = HTTPData()
+request = RequestData(http_data, (), {})
 
 __version__ = '0.1.0'
