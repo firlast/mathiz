73294bd214110a28843e482f2e85ff73e02a46f6
Jaedson Silva <contacanal798@gmail.com>
2022-11-30 21:55:50 -0300

    update project requirements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version='0.2.2',
     url='https://github.com/firlast/mathiz',
     packages=['mathiz'],
-    install_requires=['wsblib==0.7.0'],
+    install_requires=['wsblib==0.7.0', 'cryptography==38.0.4'],
     keywords=['www', 'web', 'server', 'http', 'framework', 'wsblib'],
     classifiers=[
         'License :: OSI Approved :: BSD License',
