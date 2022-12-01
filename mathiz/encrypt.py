ec18bae71c42119a30a128195d7d5f9f8da861ba
Jaedson Silva <contacanal798@gmail.com>
2022-11-30 22:11:51 -0300

    creating Fernet instance in __init__ method

--- mathiz/encrypt.py
+++ mathiz/encrypt.py
@@ -1,8 +1,14 @@
 import hashlib
 from base64 import urlsafe_b64encode
 
+from cryptography.fernet import Fernet
+
 
 class EncyptCookies:
+    def __init__(self, secret_key: str) -> None:
+        key = self._transform_key(secret_key)
+        self._fernet = Fernet(key)
+
     @staticmethod
     def _transform_key(key: str) -> bytes:
         key_hash = hashlib.md5(key.encode()).hexdigest()
