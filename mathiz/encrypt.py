fdf0010fca1e4223d060ec7647fb61bbd62b5a70
Jaedson Silva <contacanal798@gmail.com>
2022-12-04 21:18:44 -0300

    fix cookie encrypt

--- mathiz/encrypt.py
+++ mathiz/encrypt.py
@@ -15,14 +15,20 @@ class EncryptCookies:
         key_b64 = urlsafe_b64encode(key_hash.encode())
         return key_b64
 
-    def encrypt(self, cookies: dict) -> dict:
-        encrypted_cookies = {}
+    def encrypt(self, cookies: list) -> list:
+        encrypted_cookies = []
 
-        for name, value in cookies.items():
+        for cookie in cookies:
+            value = cookie['value']
+
             encrypted_value = self._fernet.encrypt(value.encode())
             encrypted_value = encrypted_value.decode()
             encrypted_value = encrypted_value.replace('=', '')
-            encrypted_cookies[name] = encrypted_value
+            encrypted_cookies.append({
+                'name': cookie['name'],
+                'value': encrypted_value,
+                'attributes': cookie['attributes']
+            })
 
         return encrypted_cookies
 
